4d5fbbda18b77c5310a4c1bfdcc69e2393a5bc64
James <wilfongjt@gmail.com>
2018-05-10 04:49:42 -0400

    added first version of specials list

--- lb-app/src/components/Home.vue
+++ lb-app/src/components/Home.vue
@@ -7,15 +7,32 @@
       <li>Remove instructions for production</li>
       <li>source: Home.vue</li>
     </ul>
+    <b-table striped hover :items="items"></b-table>
 
   </div>
 </template>
 
 <script>
+/*
+const items = [
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' },
+];
+*/
+
+
 export default {
   name: 'lb-home',
   data() {
     return {
+      items: [
+        { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' },
+      ],
       msg: 'Home',
     };
   },
