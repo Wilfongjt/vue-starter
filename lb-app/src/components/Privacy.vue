dd3baa750efa7915933e2f1f50c7ea4df86e2516
James <wilfongjt@gmail.com>
2018-05-14 10:55:37 -0400

    add terms to data, connect data to  host.policies.terms, add created()

--- lb-app/src/components/Privacy.vue
+++ lb-app/src/components/Privacy.vue
@@ -1,24 +1,47 @@
 <template>
   <div class="lb-privacy">
 
-    <h1>{{ msg }}</h1>
-    <h1>Coding Instructions</h1>
-    <ul>
-      <li>Remove instructions for production</li>
-      <li>source: Privacy.vue</li>
-      <li>Set expectations for security and user privacy.</li>
-    </ul>
+    <h1>{{ privacy.name }}</h1>
 
+    <ul>
+      <li v-for="item in privacy.policy"><h3>{{item.title}}</h3><h4>{{item.body}}</h4></li>
+
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'lb-privacy',
   data() {
     return {
-      msg: 'Privacy Policy',
+      privacy: {
+        name: 'Privacy Policy',
+        policy: [],
+      },
+      errors: [],
     };
+  },
+  created() {
+    axios.get('http://localhost:4000/host')
+    .then((response) => {
+      // JSON responses are automatically parsed.
+      // eslint-disable-next-line
+      console.log('hi');
+
+      // eslint-disable-next-line
+      console.log(response.data);
+
+      this.privacy.policy = response.data.policies.privacy;
+      // eslint-disable-next-line
+      console.log(this.privacy.policy);
+    }).catch((e) => {
+      this.errors.push(e);
+      // eslint-disable-next-line
+      console.log(e);
+    });
   },
 };
 </script>
